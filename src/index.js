f5caabd37624afec8a24f48856fa5a35831d354e
CarlosCL98 <camer.cl98@gmail.com>
2019-09-29 16:38:00 -0500

    Cambiando los archivos necesarios para que la app sea una PWA.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>,
     document.getElementById('root')
@@ -13,4 +14,4 @@ ReactDOM.render(<App/>,
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+serviceWorker.register();
